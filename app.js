ff3afb8db00a5554e47ced5b6e6c925c1df57bca
ozanmrt <ozan_akdiken@hotmail.com>
2024-09-22 17:04:21 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const fn = function () {
-  console.log('sa');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+const photo = {
+  id: 1,
+  name: 'Photo Name',
+  description: 'Photo Description',
 };
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/temp/index.html');
+});
 
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
