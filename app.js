571523ae2cc49dfbeea9a59024e0a42c7a0c78a4
ozanmrt <ozan_akdiken@hotmail.com>
2024-09-28 16:36:52 +0300

    mongoose and ejs

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 const express = require('express');
+const ejs = require('ejs');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const photo = {
-  id: 1,
-  name: 'Photo Name',
-  description: 'Photo Description',
-};
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/temp/index.html');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { photos });
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
+
+
+// 404 - Bilinmeyen sayfa için middleware
+app.use((req, res) => {
+  res.status(404).send('HATA'); // Bilinmeyen bir sayfa istenirse bu çalışır
+});
+
 
 const port = 3000;
 
